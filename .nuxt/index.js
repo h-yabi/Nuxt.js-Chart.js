63506d8233ca4028f6f3e91cb448f18a26895948
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-05-10 08:54:11 +0900

    dist 更新

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -83,7 +83,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"title":"【東京都】\u003Cbr\u003E新型コロナ陽性患者数の集計\u003Cspan\u003E（直近30日）","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+    head: {"title":"【東京都】新型コロナ陽性患者数の集計（直近30日）","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
     store,
     router,
